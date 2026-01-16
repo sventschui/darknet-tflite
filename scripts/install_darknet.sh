194b8e32a48b3516a59855bd60aa1bb7f8cde31f
Sven Tschui <sventschui@gmail.com>
2026-01-16 08:48:06 +0100

    Install L4 deb also for A100/H100 GPUs

--- scripts/install_darknet.sh
+++ scripts/install_darknet.sh
@@ -11,8 +11,25 @@ fi
 if [[ ! -d '/usr/lib64-nvidia' ]]; then
     cpu_gpu="cpu"
 else
-    gpu_name="$(nvidia-smi --query-gpu=name --format=csv,noheader | sed 's|NVIDIA\s*||' | sed 's|\s\s*|-|g')"
-    cpu_gpu="gpu-${gpu_name}"
+    gpu_name=$(nvidia-smi --query-gpu=name --format=csv,noheader | head -n1)
+
+    case "$gpu_name" in
+    *T4*)
+        gpu_class="T4"
+        ;;
+    *L4*|*A100*|*H100*)
+        gpu_class="L4"
+        ;;
+    *)
+        echo "Unsupported GPU: $gpu_name"
+        exit 1
+        ;;
+    esac
+
+    echo "Detected GPU: $gpu_name"
+    echo "GPU class: $gpu_class"
+
+    cpu_gpu="gpu-${gpu_class}"
 fi
 arch="$(dpkg --print-architecture)"
 deb_file="darknet-${DARKNET_VERSION}-Linux-${arch}-${cpu_gpu}.deb"
